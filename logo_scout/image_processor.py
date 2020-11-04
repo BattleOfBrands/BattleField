bd65b9345dc70a7ca1e74759672609ee36574f98
hareesh <hareesh@SnowBall.local>
2020-11-04 14:42:25 +0530

    got boxes

--- logo_scout/image_processor.py
+++ logo_scout/image_processor.py
@@ -1,25 +1,30 @@
 from random import randint
-
+import glob
+from logo_scout.os2d.few_shot_detection import FewShotDetection
+import logging
 
 class ImageProcessor:
     def __init__(self):
-        pass
+        self.logo_identification = self.set_up("dream11")
+        self.detect_logos()
 
-    def detect_logos(self, image):
+    def set_up(self, brand_name):
+        logo_paths = glob.glob("/Users/hareesh/Timbuctoo/BattleOfBrands/dataset/logos/training/" + brand_name + "/*.png")
+
+        return FewShotDetection(logo_paths)
+
+    def get_bounding_boxes(self, boxes):
+        return boxes.bbox_xyxy
+
+    def detect_logos(self, image=None):
         """
 
         :param image:
         :return:
         """
-        return [
-            {
-                "name": "Microsoft",
-                "visibility": 100,
-                "bounding_box": {
-                    "x": randint(0, 1024),
-                    "y": randint(0, 768),
-                    "w": randint(20, 100),
-                    "h": randint(20, 50)
-                }
-            }
-        ]
+        image_paths = glob.glob("/Users/hareesh/Timbuctoo/BattleOfBrands/dataset/match/*.jpg")
+
+        boxes = self.logo_identification.identify_logos(image_paths[1])
+        print(self.get_bounding_boxes(boxes))
+
+image_processor = ImageProcessor()
