b55a93c225e321c67e734d810b081b123be0cf70
hareesh <hareesh@SnowBall.local>
2020-11-09 19:06:45 +0530

    write file to json in drive

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ DATASET = "tests/test_data/match_images/*.png"
 DATASET = "/content/drive/My Drive/BattleofBrands/Dataset/SampleMatch/data_31/*.jpg"
 LOGOS_PATH = "tests/test_data/logos/"
 SAVE_TO = "report.json"
+SAVE_TO = "/content/drive/My Drive/BattleofBrands/Dataset/logos/cred.json"
 
 PREDICTED_LOGO_PATH = "images/"
 PREDICTED_LOGO_PATH = "/content/drive/My Drive/BattleofBrands/Dataset/logos/predicted/"
